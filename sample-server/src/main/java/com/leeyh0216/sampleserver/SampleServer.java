7ff67556197a85dda923144fd404f22bb20fc53f
leeyonghwan <leeyh0216@gmail.com>
2018-11-10 19:07:55 +0900

    Project Setting 및 Sample, Zuul 서버 추가
    - Gradle Multi Project Setting
    - Sample Server 기본 기능 구현
    - Zuul Server 기본 기능 구현

---
+++ sample-server/src/main/java/com/leeyh0216/sampleserver/SampleServer.java
@@ -0,0 +1,25 @@
+package com.leeyh0216.sampleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SampleServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleServer.class, args);
+    }
+
+    @GetMapping("/")
+    public String helloworld(){
+        return "Hello World";
+    }
+
+    @GetMapping("/bye")
+    public String bye(){
+        return "Good Bye";
+    }
+}
---
+++ zuul-server/src/main/java/com/leeyh0216/zuulserver/ZuulServer.java
@@ -0,0 +1,14 @@
+package com.leeyh0216.zuulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServer {
+
+    public static void main(String[] args){
+        SpringApplication.run(ZuulServer.class, args);
+    }
+}
